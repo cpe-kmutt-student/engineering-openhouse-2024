0c1db1ea458b6e1cecd2bfc2a68808a82fd9608b
Mirailisc <mirailisclm@gmail.com>
2023-09-16 16:14:52 +0700

    chore: re-gen lockfile

--- src/components/Hello/index.tsx
+++ src/components/Hello/index.tsx
@@ -1,16 +1,12 @@
-import { useTranslation } from 'react-i18next'
-import { Translation } from '../../services/translation'
 import { useState } from 'react'
 
 const Hello: React.FC = (): JSX.Element => {
   const [name, setName] = useState<string>('')
 
-  const { t } = useTranslation()
-
   return (
     <>
       <input type="text" name="name" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)} />
-      <h1>{t(Translation.hello, { name })}</h1>
+      <h1>Hello, {name}</h1>
     </>
   )
 }
