efd5ec61485775c85f6390878e8f8cd825ec6011
Mirailisc <mirailisclm@gmail.com>
2023-09-20 22:11:09 +0700

    chore: set image slide same resolution

--- src/contents/imageSlide.ts
+++ src/contents/imageSlide.ts
@@ -1,6 +1,5 @@
 export const imageSlide = [
-  'https://images.hdqwalls.com/wallpapers/blue-archive-hm.jpg',
-  'https://www.charlieintel.com/cdn-cgi/image/width=3840,quality=75,format=auto/https://editors.charlieintel.com/wp-content/uploads/2022/10/best-characters-tier-list-blue-archive-1.jpg',
-  'https://media.pocketgamer.com/artwork/na-33404-1636469504/blue-archive-screenshot-2_jpg_1640.jpg',
-  'https://s.isanook.com/ga/0/ud/234/1171202/blue_archive_anime_cover.jpg',
+  'https://initiate.alphacoders.com/images/119/cropped-1200-600-1198959.png?1847',
+  'https://initiate.alphacoders.com/images/120/cropped-1200-600-1201376.jpg?3208',
+  'https://initiate.alphacoders.com/images/130/cropped-1200-600-1309668.jpg?6102',
 ]
