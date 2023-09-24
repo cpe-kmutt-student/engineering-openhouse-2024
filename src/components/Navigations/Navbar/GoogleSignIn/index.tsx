67a4e584812faf0e23a69138a3aaf7ac1f55520b
Mirailisc <mirailisclm@gmail.com>
2023-09-24 19:16:56 +0700

    chore: mock up cookie handler

--- src/components/Navigations/Navbar/GoogleSignIn/index.tsx
+++ src/components/Navigations/Navbar/GoogleSignIn/index.tsx
@@ -1,6 +1,8 @@
 import { Button } from 'antd'
 import { axiosInstance } from '../../../../utils/axios'
 import { useNavigate } from 'react-router-dom'
+import { useCookies } from 'react-cookie'
+import { ACCESS_TOKEN } from '../../../../configs/cookie'
 
 interface Props {
   continuePath: string | null
@@ -9,11 +11,14 @@ interface Props {
 const GoogleSignIn: React.FC<Props> = ({ continuePath }: Props): JSX.Element => {
   const navigate = useNavigate()
 
+  const [cookie, setCookie] = useCookies([ACCESS_TOKEN])
+
   const handleSignInWithGoogle = async () => {
     // TODO: Sign in with Google & handle access_token cookie
 
     const res = await axiosInstance.get('/api/auth/google')
     console.log(res)
+    // setCookie(ACCESS_TOKEN, res.data.access_token)
 
     if (continuePath) {
       navigate(continuePath)
