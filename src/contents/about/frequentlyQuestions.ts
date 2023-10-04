807867d744c71ba5a82ec6b1edaa80a615b33002
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-10-04 22:58:09 +0700

    chore: add Event and FAQ data

--- src/contents/about/frequentlyQuestions.ts
+++ src/contents/about/frequentlyQuestions.ts
@@ -7,36 +7,37 @@ export const frequentlyQuestions: IQandA[] = [
   {
     question: 'Openhouse มีอะไรให้ทำบ้าง ?',
     answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+      'ตลอด 3 วันคณะวิศวกรรมศาสตร์ใน มจธ. จัดกิจกรรม Open house เปิดโอกาสให้น้อง ๆ ได้ร่วมเวิร์คช็อปในแต่ละภาค เพื่อสัมผัสประสบการณ์เรียนวิศวกรรมในแต่ละสภาพแวดล้อม ยังสามารถมีกิจกรรมพี่พาน้องทัวร์ไปเยี่ยมชมภาคต่าง ๆ น้อง ๆ สามารถร่วมกิจกรรม E-stamp เพื่อแลกของที่ระลึกสุดพิเศษได้อีกด้วย',
   },
   {
     question: 'กิจกรรม Workshop มีการลงทะเบียนหน้างานแบบ Walk-in หรือไม่ ?',
-    answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+    answer: 'ลงทะเบียนหน้างานเเบบ Walk-in เท่านั้น',
   },
   {
     question: 'เดินทางมายังไง ?',
-    answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+    answer: 'สามารถไปดูวิธีการเดินทางที่ "ช่องทางการเดินทาง" ได้เลยยย',
   },
   {
     question: 'เตรียมตัวยังไง เอาอะไรไปบ้าง ?',
-    answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+    answer: 'โทรศัพท์มือถือที่ติดตัวได้ เพราะกิจกรรมของเราจะมีการเก็บ E-Stamp ผ่านเว็บไซต์ เพื่อแลกของที่ระลึกสุดพิเศษ',
   },
   {
     question: 'แต่งกายอย่างไรดี ?',
     answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+      'ชุดนักเรียนหรือชุดสุภาพ (กางเกงขายาว รองเท้าหุ้มส้น) ไม่ควรใส่เสื้อแขนกุด เสื้อสายเดี่ยว เสื้อเอวลอย กางเกงขาสั้น หรือรองเท้าแตะ',
   },
   {
     question: 'ผู้ปกครอง หรือ คุณครูเข้าร่วมกิจกรรมได้หรือไม่ ?',
-    answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.mIpsum',
+    answer: 'ผู้ปกครอง คุณครู หรือบุคคลภายนอกที่สนใจสามารถลงทะเบียนเพื่อเข้าร่วมกิจกรรมต่าง ๆ ได้',
   },
   {
     question: 'มีประกาศนียบัตรหรือไม่ ?',
     answer:
-      'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.',
+      'มีประกาศนียบัตรให้เเต่ต้องเป็นไปตามเงื่อนไขของกิจกรรม โดยเงื่อนไขการได้รับ คือมาลงทะเบียนเข้าร่วมที่จุดลงทะเบียนวันงาน นั่นเอง',
+  },
+  {
+    question: 'กิจกรรมพี่พาน้องทัวร์ต่างจาก workshop อย่างไร',
+    answer:
+      'workshop คือกิจกรรมที่น้องได้ลงมือปฏิบัติแลบขอบแต่ละภาคซึ่งมาจากเนื้อหาที่ได้มีการสอนจริง แต่ปรับให้มีสนุกสนานมากขึ้น พี่พาน้องทัวร์ คือกิจกรรมที่พี่ ๆ พาน้องเยี่ยมชมแต่ละภาค เพื่อแนะนำให้น้อง ๆ นั้นรู้จักภาควิชาที่ได้ไปเยี่ยมชมมากขึ้น',
   },
 ]
--- src/contents/eventSchedules.ts
+++ src/contents/eventSchedules.ts
@@ -6,39 +6,78 @@ export interface IEventSchedule {
 
 export const eventSchedules: IEventSchedule[] = [
   {
-    title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'พิธีเปิดกิจกรรม Engineering Open house Canival at Bangmod',
     date: '10/14/2023',
-    time: '09:00-12:00',
+    time: '08:30-9:00',
   },
   {
-    title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'กิจกรรมการแข่งขันนวัตกรรมฯ รอบชิงชนะเลิศ รอบเช้า',
     date: '10/14/2023',
     time: '12:00-16:00',
   },
   {
-    title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'ซุ้มขายของ',
+    date: '10/14/2023',
+    time: '09:00-17:00',
+  },
+  {
+    title: 'กิจกรรมการแข่งขันนวัตกรรมฯ รอบชิงชนะเลิศ รอบบ่าย',
+    date: '10/14/2023',
+    time: '13:00-15:30',
+  },
+  {
+    title: 'Folk song(1)',
     date: '10/15/2023',
-    time: '09:00-12:00',
+    time: '10:00-11:30',
   },
   {
-    title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'ซุ้มขายของ',
     date: '10/15/2023',
-    time: '12:00-16:00',
+    time: '9:00-17:00',
   },
   {
-    title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    date: '10/16/2023',
-    time: '09:00-12:00',
+    title: 'Talk show',
+    date: '10/15/2023',
+    time: '11.30-12.30',
   },
   {
-    title:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    title: 'Folk song(2)',
+    date: '10/15/2023',
+    time: '12.30-13.00',
+  },
+  {
+    title: 'กิจกรรม Engineering Quest รอบชิงชนะเลิศ',
+    date: '10/15/2023',
+    time: '13.30-17.00',
+  },
+  {
+    title: 'Talk show',
     date: '10/16/2023',
-    time: '12:00-16:00',
+    time: '10.30-11.00',
+  },
+  {
+    title: 'Folk song(3)',
+    date: '10/16/2023',
+    time: '11.30-13.00',
+  },
+  {
+    title: 'Talk show',
+    date: '10/16/2023',
+    time: '13.00-14.00',
+  },
+  {
+    title: 'พิธีปิดงาน Open house',
+    date: '10/16/2023',
+    time: '14.00-14.30',
+  },
+  {
+    title: 'ซุ้มขายของ',
+    date: '10/16/2023',
+    time: '09.00-17.00',
+  },
+  {
+    title: 'คอนเสิร์ต',
+    date: '10/16/2023',
+    time: '16.00-18.30',
   },
 ]
