4722a54043012d8a1b0eefb26a1de780ed52a39e
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-10-10 15:26:03 +0700

    fix: education when edit profile & add event

--- src/utils/gaEvents.ts
+++ src/utils/gaEvents.ts
@@ -1,3 +1,4 @@
 import ReactGA from 'react-ga4'
 
-export const registerEvent = () => ReactGA.event('Register')
+export const registerEvent = () => ReactGA.event('Register') // when user regis
+export const buttonEvent = (name: string) => ReactGA.event('button-' + name)
