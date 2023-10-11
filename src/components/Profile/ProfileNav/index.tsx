c0afff1390697b11ea5a25700108bcd3f1da9e39
ChaiyapatOam <chaiyapatoam12@gmail.com>
2023-10-11 15:53:23 +0700

    chore: navbar

--- src/components/Profile/ProfileNav/index.tsx
+++ src/components/Profile/ProfileNav/index.tsx
@@ -5,8 +5,7 @@ import { PROFILE_PATH, STAMP_PATH } from 'src/configs/routes'
 const ProfileNav: React.FC = (): JSX.Element => {
   return (
     <div className={styles.nav}>
-      <Link to={PROFILE_PATH}>โปรไฟล์</Link>
-      <Link to={STAMP_PATH}>E-Stamp</Link>
+      <Link to={PROFILE_PATH}>โปรไฟล์</Link>|<Link to={STAMP_PATH}>E-Stamp</Link>
     </div>
   )
 }
