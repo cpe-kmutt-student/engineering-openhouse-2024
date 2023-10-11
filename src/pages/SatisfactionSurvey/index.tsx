4502da2cb6a110aca4e317d38abbe1a720df319b
SSSBoOm <boom1909chanapat@gmail.com>
2023-10-11 14:42:23 +0700

    feat: change google form

--- src/pages/SatisfactionSurvey/index.tsx
+++ src/pages/SatisfactionSurvey/index.tsx
@@ -5,9 +5,7 @@ import { useContext } from 'react'
 const SatisfactionSurvey: React.FC = (): JSX.Element => {
   const auth = useContext(AuthContext)
 
-  const FORM_URL = `
-  https://docs.google.com/forms/d/e/1FAIpQLSfaaSnFkJju0_NtdBpGX_GKN2j6BzbuykYIbRmCw8CQlyz3vw/viewform?usp=pp_url&entry.40952531=${auth?.authContext.email}`
-
+  const FORM_URL = `https://docs.google.com/forms/d/e/1FAIpQLSdcKt92D3g94JaBbK0VdrvMQPVXu8Tln43FmRI053YsvrV7Sw/viewform?usp=pp_url&entry.1388905119=${auth?.authContext.email}`
   return (
     <div className={styles.survey}>
       <iframe
